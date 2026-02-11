f4c6e8e37cc1d0802dca5611b55c16dbdb24515c
smallclover <18363998103@163.com>
2026-02-11 18:49:42 +0800

    代码优化

--- src/model/state.rs
+++ src/model/state.rs
@@ -1,6 +1,5 @@
 use std::path::PathBuf;
 use std::sync::mpsc::{self, Receiver, Sender};
-use egui::{Pos2, TextureHandle, Rect};
 use crate::ui::components::{
     toast::{ToastManager, ToastSystem},
     ui_mode::UiMode
@@ -12,24 +11,6 @@ pub enum ViewMode {
     Grid,
 }
 
-#[derive(Clone)]
-pub struct MonitorTexture {
-    pub id: usize,
-    pub rect: Rect,
-    pub texture: TextureHandle,
-}
-
-#[derive(Clone, Default)]
-pub struct CropState {
-    pub is_active: bool,
-    pub start: Option<Pos2>,
-    pub current: Option<Pos2>,
-    pub was_maximized: bool,
-    pub was_decorated: bool,
-    pub monitor_textures: Vec<MonitorTexture>,
-    pub offset: Pos2, // 记录全屏窗口相对于虚拟屏幕原点的偏移
-}
-
 pub struct ViewState {
     pub ui_mode: UiMode,
     pub view_mode: ViewMode,
@@ -37,7 +18,6 @@ pub struct ViewState {
     pub path_receiver: Receiver<PathBuf>,
     pub toast_system: ToastSystem,
     pub toast_manager: ToastManager,
-    pub crop_state: CropState,
 }
 
 impl Default for ViewState {
@@ -53,7 +33,6 @@ impl Default for ViewState {
             path_receiver,
             toast_system,
             toast_manager,
-            crop_state: CropState::default(),
         }
     }
 }
